d45834c17c417fed2e5219cdc890b4a0b826d015
Oluwole Ayomide Godwin <ayomideoluwole91@gmail.com>
2025-10-27 14:48:00 +0100

    Refactor mobile menu toggle and product display logic

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ function displayProducts(list) {
           <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
         </svg>
       </div>
-      <img src="${prod.img}" alt="${prod.name}">
+      <img src="${prod.img}" alt="${prod.name}" loading="lazy">
       <div class="product-info">
         <h4>${prod.name}</h4>
         <p>₦${prod.price.toLocaleString()}</p>
